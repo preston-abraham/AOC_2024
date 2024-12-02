f6264e270c3c64f324d32b778ef4996ce1258812
Preston Abraham <112017483+preston-abraham@users.noreply.github.com>
2024-12-02 16:41:34 -0600

    Update day1.py
    Re-factor day 1

--- day1.py
+++ day1.py
@@ -1,21 +1,8 @@
 with open("inputs/day1.txt",'r') as f:
     full_inp = f.read()
 
-inp = [i.split('   ') for i in full_inp.split('\n')]
-inp = [[int(i) for i in j] for j in inp]
-l1 = sorted([e[0] for e in inp])
-l2 = sorted([e[1] for e in inp])
+inp = list(map(lambda x:list(map(int,x.split())), full_inp.split('\n')))
+l1,l2 = list(map(sorted,[[e[i] for e in inp] for i in range(2)]))
 
-s = 0
-for i in range(len(l2)):
-    s+= abs(l1[i] - l2[i])
-print(s)
-
-s2 = 0
-for e in l1:
-    sc = 0
-    for e2 in l2:
-        if e == e2:
-            sc += e
-    s2 += sc
-print(s2)
+print(sum(list(map(lambda x,y: abs(x-y),l1,l2))))
+print(sum(list(map(lambda x,y: x*y.count(x),l1,repeat(l2)))))
